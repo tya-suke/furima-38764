c4b40f16da5bdb91a5ee5a064d350c79b02f2dc5
tya-suke <m.keisuke22@gmail.com>
2023-01-07 21:08:32 +0900

    Revert "ログイン、販売中商品"
    This reverts commit b1872b92d4272dd9b6eb318601c9e894bd9be35b.

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -5,8 +5,8 @@ class DaysToShip < ActiveHash::Base
     { id: 2, name: '2~3日で発送' },
     { id: 3, name: '4~7日で発送' }
   ]
+end
 
   include ActiveHash::Associations
-  has_many :products
-end
+  # belongs_to :user
 
