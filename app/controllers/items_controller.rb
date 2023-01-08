79e7a5f2c0e10bc3c4a8e02dd1f3566260468d62
tya-suke <m.keisuke22@gmail.com>
2023-01-08 18:12:40 +0900

    商品情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,7 @@ class ItemsController < ApplicationController
    before_action :authenticate_user!,except: [:index, :show]
    
   def index
-
   @items = Item.order("created_at DESC")
-
   end
 
   def new
@@ -22,10 +20,21 @@ class ItemsController < ApplicationController
   end
 
   def show
-
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   private
   def item_params
