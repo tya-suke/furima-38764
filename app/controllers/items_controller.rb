246d33b92b55f563444450e3ff26e69498a5fda3
tya-suke <m.keisuke22@gmail.com>
2023-01-08 21:20:16 +0900

    課題実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-
-   before_action :authenticate_user!,except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
    
   def index
   @items = Item.order("created_at DESC")
@@ -25,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveHash::Base
 
   self.data = [
+    { id: 0, category: '---' },
     { id: 1, category: 'レディース' },
     { id: 2, category: 'メンズ' },
     { id: 3, category: 'ベビー・キッズ' },
